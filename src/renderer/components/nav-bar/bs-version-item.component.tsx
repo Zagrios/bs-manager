74add7d9c881b70f5cbf2997d5d0361bc0075e65
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-05-17 23:51:51 +0200

    fit icon, et ajout icon steam

--- src/renderer/components/nav-bar/bs-version-item.component.tsx
+++ src/renderer/components/nav-bar/bs-version-item.component.tsx
@@ -1,12 +1,14 @@
 import { BSVersion } from "main/services/bs-version-manager.service";
-import bsNote from '../../../../assets/bs-note-fill.svg'
 import { Link } from "react-router-dom";
+import { BsNoteFill } from "../svgs/bs-note-fill.component";
+import { SteamIcon } from "../svgs/steam-icon.component";
 
 export default function BsVersionItem(props: {version: BSVersion}) {
   return (
-    <div className='cursor-pointer flex'>
-        <img src={bsNote} alt="" className="inline w-7 aspect-square min-w-0" />
-        <Link to={`/bs-version/${props.version.BSVersion}`} state={props.version} className="inline w-max text-lg text-gray-200 font-bold min-w-0">{props.version.BSVersion}</Link>
+    <div className='flex cursor-pointer w-max justify-center content-center items-center pr-1 text-red-600'>
+      {props.version.steam && <SteamIcon className="w-[19px] h-[19px] mr-1"/>}
+      {!props.version.steam && <BsNoteFill className="w-[22px] h-[22px] mr-1"/>}
+      <Link to={`/bs-version/${props.version.BSVersion}`} state={props.version} className="shrink-0 text-lg text-gray-200 font-bold min-w-0">{props.version.BSVersion}</Link>
     </div>
   )
 }
