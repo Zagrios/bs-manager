225150126cca0b739e601d8f1d1409627d977deb
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-05-17 23:52:26 +0200

    icon note beat saber

---
+++ src/renderer/components/svgs/bs-note-fill.component.tsx
@@ -0,0 +1,8 @@
+
+
+export function BsNoteFill({className}: {className: string}) {
+
+  return (
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 541.867 541.867" height="2048" width="2048"><g transform="translate(0 244.867)"><rect rx="69.453" y="-177.133" x="67.733" height="406.4" width="406.4" fill="currentColor" fillRule="evenodd" paintOrder="fill markers stroke"/><path d="M135.467-109.4H406.4v33.867L270.933-7.8 135.467-75.533z" fill="white" /></g></svg>
+  ) 
+}
