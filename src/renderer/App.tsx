636f9fe9a2f720fdfe3b8b006d8422714467fe67
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-05-22 22:55:20 +0200

    remove redux

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -2,28 +2,11 @@ import { NavBar } from "./components/nav-bar/nav-bar.component";
 import TitleBar from "./components/title-bar/title-bar.component";
 import { Routes, Route } from "react-router-dom";
 import { AvailableVersionsList } from "./pages/available-versions-list.components";
-import { useDispatch, useSelector } from "react-redux";
-import { useEffect } from "react";
 import { VersionViewer } from "./pages/version-viewer.component";
 import { Modal } from "./components/modal/modal.component";
 
 export default function App() {
-
-  const dispatch = useDispatch()
-
-  useEffect(() => {
-    initIpcsListener();
-    window.electron.ipcRenderer.sendMessage('bs-version.installed-versions');
-    window.electron.ipcRenderer.sendMessage('bs-version.request-versions');
-  }, [])
-
-  const initIpcsListener= () => {
-    window.electron.ipcRenderer.on('bs-version.installed-versions', versions => dispatch({type: 'INSTALLED_BS_ADD_ALL', payload: versions}));
-    window.electron.ipcRenderer.on('bs-version.request-versions', versions => dispatch({type: 'AVAILABLE_BS_INIT', payload: versions}));
-  }
   
-  
-
   return (
     <div className="relative w-screen h-screen overflow-hidden flex dark:bg-main-color-1 z-0">
       <Modal/>
--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,21 +1,16 @@
 import { createRoot } from 'react-dom/client';
-import { InitDownloadInfoInterface } from '../main/ipcs/bs-download-ipcs';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import 'tailwindcss/tailwind.css';
 import './index.css'
-import { Provider } from 'react-redux';
-import store from './store/store'
 
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
-  <Provider store={store}>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </Provider>
+  <HashRouter>
+    <App />
+  </HashRouter>
 );
 
 // setTimeout(() => {
