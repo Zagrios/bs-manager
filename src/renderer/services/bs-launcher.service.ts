3bd2bd0ab0984804587a24a82e783370013598c3
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-05-26 22:38:02 +0200

    service for BS launch, will be usefull in future

---
+++ src/renderer/services/bs-launcher.service.ts
@@ -0,0 +1,32 @@
+import { LauchOption } from "main/ipcs/bs-launcher-ipcs";
+import { BSVersion } from "../../main/services/bs-version-manager.service";
+
+export class BSLauncherService{
+
+    private static instance: BSLauncherService;
+
+    public static getInstance(){
+        if(!BSLauncherService.instance){ BSLauncherService.instance = new BSLauncherService(); }
+        return BSLauncherService.instance;
+    }
+
+    private constructor(){}
+
+    public launch(version: BSVersion, oculus: boolean, desktop: boolean, debug: boolean): Promise<any>{
+        return new Promise((resolve, reject) => {
+
+            window.electron.ipcRenderer.on('bs-launch.launch', res => {
+                resolve(res);
+            });
+            const lauchOption: LauchOption = {debug, oculus, desktop, version: version}
+            window.electron.ipcRenderer.sendMessage('bs-launch.launch', lauchOption);
+        })
+    }
+
+}
+
+export enum LaunchResult {
+    LAUNCHED = 0,
+    STEAM_NOT_RUNNING = 1,
+    EXE_NOT_FIND = 2,
+}
