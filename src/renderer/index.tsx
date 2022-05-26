b93a3ad8ddb7208a16d69bc69dae28d55ec8a243
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-05-26 22:32:49 +0200

    remove comms

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -12,16 +12,3 @@ root.render(
     <App />
   </HashRouter>
 );
-
-// setTimeout(() => {
-//   window.electron.ipcRenderer.sendMessage('bs-download.start', {app: '620980', depot: '620981', manifest: '6222769774084748916', stay: true, username: 'lekilleur618', folder: 'test2', cwd: 'C:\\test'} as InitDownloadInfoInterface);
-// }, 5000);
-
-// window.electron.ipcRenderer.on('bs-download.ask-password', () => console.log('omg popup password'));
-
-setTimeout(() => {
-  //window.electron.ipcRenderer.sendMessage('test-launch', null);
-  //window.electron.ipcRenderer.sendMessage('bs-version.request-versions', null);
-}, 5000);
-
-//window.electron.ipcRenderer.on('bs-version.request-versions', (v) => console.log(v));
