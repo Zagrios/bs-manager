24113929a2253f0fb9fe857f670b59172edd6764
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-06-06 23:38:15 +0200

    add padding bottom

--- src/renderer/components/available-versions/available-versions-slide.component.tsx
+++ src/renderer/components/available-versions/available-versions-slide.component.tsx
@@ -22,7 +22,7 @@ export function AvailableVersionsSlide(props: {year: string}) {
 
   return (
     <div className="w-full max-w-full max-h-full shrink-0 flex items-start justify-center overflow-x-hidden overflow-y-scroll content-start scrollbar-thin hover:scrollbar-thumb-neutral-900">
-      <div className="relative left-[2px] flex justify-center items-start content-start flex-wrap max-w-6xl">
+      <div className="relative left-[2px] flex justify-center items-start content-start flex-wrap max-w-6xl pb-4">
         {availableVersions.map((version, index) =>
           <AvailableVersionItem key={index} version={version}></AvailableVersionItem>
         )}
