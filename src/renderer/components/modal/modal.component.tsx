d933db3a034f3076ca971e303bb558e008db6f74
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-06-12 23:01:29 +0200

    remove useless import and change background opacity

--- src/renderer/components/modal/modal.component.tsx
+++ src/renderer/components/modal/modal.component.tsx
@@ -1,7 +1,6 @@
 import { ModalExitCode, ModalService, ModalType } from "renderer/services/modale.service";
 
 import { useEffect, useState } from "react"
-import { distinctUntilChanged } from "rxjs";
 
 import { LoginModal } from "./modal-types/login-modal.component";
 import { GuardModal } from "./modal-types/guard-modal.component";
@@ -21,7 +20,7 @@ export function Modal() {
 
   return (
     <div className={`absolute w-screen h-screen flex content-center items-center justify-center z-50 ${modalType ? "top-0" : "top-[calc(100%+50px)]"}`}>
-      <span onClick={() => modalSevice.resolve({exitCode: ModalExitCode.NO_CHOICE})} className={`absolute top-0 bottom-0 right-0 left-0 transition-opacity bg-black ${modalType? "opacity-40" : "opacity-0"}`}></span>
+      <span onClick={() => modalSevice.resolve({exitCode: ModalExitCode.NO_CHOICE})} className={`absolute top-0 bottom-0 right-0 left-0 transition-opacity bg-black ${modalType? "opacity-60" : "opacity-0"}`}></span>
       <div className={`absolute transition-transform ${modalType ? "translate-y-0" : "translate-y-full"}`}>
           {modalType === ModalType.STEAM_LOGIN && <LoginModal resolver={modalSevice.getResolver()}/>}
           {modalType === ModalType.GUARD_CODE && <GuardModal resolver={modalSevice.getResolver()}/>}
