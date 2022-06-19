9d7bc082c6c4cf5e1268476c04ce42a8c4fe651f
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-06-19 21:59:29 +0200

    not important

--- src/renderer/components/svgs/bs-note-fill.component.tsx
+++ src/renderer/components/svgs/bs-note-fill.component.tsx
@@ -4,10 +4,10 @@ export function BsNoteFill({className}: {className: string}) {
 
   return (
     <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 406.4 406.4" height="406.4" width="406.4">
-    <g>
-        <rect rx="69.453" height="406.4" width="406.4" fill="currentColor" fillRule="evenodd" paintOrder="fill markers stroke"/>
-        <path transform="translate(-70 170)" d="M135.467-109.4H406.4v33.867L270.933-7.8 135.467-75.533z" fill="white" />
-    </g>
-</svg>
+      <g>
+          <rect rx="69.453" height="406.4" width="406.4" fill="currentColor" fillRule="evenodd" paintOrder="fill markers stroke"/>
+          <path transform="translate(-70 170)" d="M135.467-109.4H406.4v33.867L270.933-7.8 135.467-75.533z" fill="white" />
+      </g>
+    </svg>
   ) 
 }
