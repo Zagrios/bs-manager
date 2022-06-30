c3043e0bb3ec625f17776b903e60b8a30a816d7f
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-06-30 21:55:51 +0200

    remove useless import

--- src/main/main.ts
+++ src/main/main.ts
@@ -9,7 +9,7 @@
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
 import path from 'path';
-import { app, BrowserWindow, dialog, ipcMain, shell } from 'electron';
+import { app, BrowserWindow, shell } from 'electron';
 import { autoUpdater } from 'electron-updater';
 import log from 'electron-log';
 import { resolveHtmlPath } from './util';
