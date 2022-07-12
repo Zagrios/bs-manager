e6a9b1ff11e3224465c079503f6c279d394ead05
MathieuG-P <mathieu.gries-peroz@cirkwi.com>
2022-07-12 21:39:30 +0200

    remove useMemo that cause some bugs

--- src/renderer/hooks/use-translation.hook.ts
+++ src/renderer/hooks/use-translation.hook.ts
@@ -1,14 +1,11 @@
-import { useMemo } from "react";
 import { I18nService } from "renderer/services/i18n.service"
 
 export function useTranslation(): (translationKey: string) => string{
-    
-    const i18nService = I18nService.getInstance();
+   
+   const i18nService = I18nService.getInstance();
 
-    return (key: string) => {
-        return useMemo(() => {
-         const tranlatables = key.split(" ");
-         return tranlatables.map((key) => i18nService.translate(key)).join(" ");
-      }, [key, i18nService.currentLanguage]);
-    }
+   return (key: string) => {
+      const tranlatables = key.split(" ");
+      return tranlatables.map((key) => i18nService.translate(key)).join(" ");
+   }
 }
