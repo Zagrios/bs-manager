af95b24005a1bc0f98fddba00d0170387431593c
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-09-28 23:14:58 +0200

    fix glowing invisible

--- src/renderer/components/version-viewer/slides/launch/launch-mod-toogle.component.tsx
+++ src/renderer/components/version-viewer/slides/launch/launch-mod-toogle.component.tsx
@@ -9,7 +9,7 @@ export function LaunchModToogle({onClick, active, text, icon}: props) {
 
     return (
         <div className={`relative rounded-full cursor-pointer group active:scale-95 transition-transform ${!active && "shadow-md shadow-black"}`} onClick={() => onClick(!active)}>
-            <div className={`absolute glow-on-hover rounded-full ${active && "opacity-100 blur-[2px]"}`}/>
+            <div className={`absolute glow-on-hover !rounded-full ${active && "!opacity-100 blur-[2px]"}`}/>
             <div className='w-full h-full pl-6 pr-6 flex justify-center items-center bg-light-main-color-2 dark:bg-main-color-2 p-3 rounded-full text-gray-800 dark:text-white group-hover:bg-light-main-color-1 dark:group-hover:bg-main-color-1'>
                 <BsmIcon icon={icon} className='mr-1 h-7 text-gray-800 dark:text-white'/>
                 <span className='w-fit min-w-fit h-full text-lg font-bold uppercase tracking-wide italic'>{t(text)}</span>
