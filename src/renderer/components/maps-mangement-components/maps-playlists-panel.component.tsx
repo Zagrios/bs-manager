cfda3830db3e3a31f89d51c43722738e45bc1403
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-11-14 00:22:43 +0100

    fix css issue

--- src/renderer/components/maps-mangement-components/maps-playlists-panel.component.tsx
+++ src/renderer/components/maps-mangement-components/maps-playlists-panel.component.tsx
@@ -15,10 +15,10 @@ export function MapsPlaylistsPanel({version, oneBlock = false}: Props) {
     return (
         <>
         {!oneBlock && <TabNavBar className="mb-8 w-72" tabsText={["Maps", "Playlists"]} onTabChange={setTabIndex}/>}
-        <div className="w-full h-full bg-main-color-2 rounded-md shadow-black shadow-md overflow-hidden">
+        <div className="w-full h-full flex flex-col bg-main-color-2 rounded-md shadow-black shadow-md overflow-hidden">
             {oneBlock && <TabNavBar className="!rounded-none shadow-sm" tabsText={["Maps", "Playlists"]} onTabChange={setTabIndex}/>}
-            <div className="w-full h-full min-h-0 flex flex-row items-center transition-transform duration-300" style={{transform: `translate(${-(tabIndex * 100)}%, 0)`}}>
-                <LocalMapsListPanel className="h-full w-full grow shrink-0 flex flex-wrap justify-center content-start gap-2 p-3" version={version}/>
+            <div className="w-full grow min-h-0 flex flex-row items-center transition-transform duration-300" style={{transform: `translate(${-(tabIndex * 100)}%, 0)`}}>
+                <LocalMapsListPanel className="w-full h-full shrink-0 flex flex-wrap justify-center content-start gap-2 p-3 overflow-y-scroll scrollbar-thin scrollbar-thumb-rounded-full scrollbar-thumb-neutral-900" version={version}/>
                 <div className="w-full h-full grow shrink-0">b</div>
             </div>
         </div> 
