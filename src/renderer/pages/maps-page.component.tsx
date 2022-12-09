4308ba5125ebdbd75f2a859faf5b14bb0c2a6161
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-12-09 23:46:20 +0100

    fix text style

--- src/renderer/pages/maps-page.component.tsx
+++ src/renderer/pages/maps-page.component.tsx
@@ -6,7 +6,7 @@ export default function MapsPage() {
         <div className="w-full h-full flex items-center flex-col pt-2">
             <Slideshow className="absolute w-full h-full top-0"/>
             <h1 className="text-gray-100 text-2xl z-[1]">Maps partagées</h1>
-            <div className="z-[1] w-full min-h-0 grow p-10 flex flex-col items-center justify-start">
+            <div className="z-[1] w-full min-h-0 grow p-10 flex flex-col items-center justify-start text-white">
                 <MapsPlaylistsPanel/>
             </div>
         </div>
