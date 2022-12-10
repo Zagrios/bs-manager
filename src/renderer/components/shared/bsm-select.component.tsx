1ca62ba2f55c8c38bd9db962930a2c15c924b773
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2022-12-10 01:29:23 +0100

    handle translation AND onChange reture string

--- src/renderer/components/shared/bsm-select.component.tsx
+++ src/renderer/components/shared/bsm-select.component.tsx
@@ -1,13 +1,16 @@
 import { ChangeEvent, CSSProperties } from "react"
+import { useTranslation } from "renderer/hooks/use-translation.hook"
 
 type Props = {
     className?: string,
     style?: CSSProperties,
     options?: BsmSelectOption[],
-    onChange?: (value: any) => void
+    onChange?: (value: string) => void
 }
 
 export function BsmSelect({className, style, options, onChange}: Props) {
+
+    const t = useTranslation();
 
     const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
         onChange?.(event.target.value);
@@ -16,7 +19,7 @@ export function BsmSelect({className, style, options, onChange}: Props) {
     return (
         <select className={className} style={style} onChange={handleChange}>
             {options && options.map(option => (
-                <option key={option.value} value={option.value}>{option.text}</option>
+                <option key={option.value} value={option.value}>{t(option.text)}</option>
             ))}
         </select>
   )
