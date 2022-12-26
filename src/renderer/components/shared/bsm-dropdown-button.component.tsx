4cb9482e27741271ea944c0f519a1d4fb6d07a93
MathieuG-P <mathieu.gries-peroz@cirkwi.com>
2022-12-26 21:50:41 +0100

    fix fake items in dropdown

--- src/renderer/components/shared/bsm-dropdown-button.component.tsx
+++ src/renderer/components/shared/bsm-dropdown-button.component.tsx
@@ -22,6 +22,8 @@ type Props = {
 
 export function BsmDropdownButton({className, items, align, withBar = true, icon = "settings", buttonClassName, menuTranslationY, children, text, textClassName}: Props) {
 
+    console.log("DROPDOWN", items);
+
    const [expanded, setExpanded] = useState(false)
    const t = useTranslation();
    const ref = useRef(null)
@@ -44,7 +46,7 @@ export function BsmDropdownButton({className, items, align, withBar = true, icon
       <div ref={ref} className={className}>
          <BsmButton onClick={() => setExpanded(!expanded)} className={buttonClassName ?? defaultButtonClassName} icon={icon} active={expanded} onClickOutside={handleClickOutside} withBar={withBar} text={text}/>
          <div className={`py-1 w-fit absolute cursor-pointer top-[calc(100%-4px)] rounded-md bg-inherit text-sm text-gray-800 dark:text-gray-200 shadow-md shadow-black transition-[scale] ease-in-out ${alignClass}`} style={{scale: expanded ? "1" : "0", translate: `0 ${menuTranslationY}`}}>
-            { items?.map((i) => (
+            { items?.map((i) => i && (
                <div key={crypto.randomUUID()} onClick={() => i.onClick?.()} className="flex w-full px-3 py-2 hover:backdrop-brightness-150">
                   {i.icon && <BsmIcon icon={i.icon} className="h-5 w-5 mr-1 text-inherit"/>}
                   <span className="w-max">{t(i.text)}</span>
