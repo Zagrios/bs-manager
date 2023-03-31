74e5f54c901555677d7d6c529e1c1728abd12889
Shido <gaetangerard@hotmail.fr>
2023-03-31 17:15:01 +0200

    finalize modal

--- src/renderer/components/modal/modal-types/changelog-modal/changelog-modal.component.tsx
+++ src/renderer/components/modal/modal-types/changelog-modal/changelog-modal.component.tsx
@@ -1,10 +1,11 @@
 import { BsmImage } from "renderer/components/shared/bsm-image.component";
 import { ModalComponent } from "renderer/services/modale.service";
 import BeatRunning from '../../../../../../assets/images/apngs/beat-running.png';
-import { BsmIcon } from "renderer/components/svgs/bsm-icon.component";
 import { useEffect, useState } from "react";
 import { I18nService } from "renderer/services/i18n.service";
 import "./changelog-modal.component.css"
+import { BsmButton } from "renderer/components/shared/bsm-button.component";
+import { LinkOpenerService } from "renderer/services/link-opener.service";
 
 const i18nService: I18nService = I18nService.getInstance();
   type jsonData = {
@@ -15,13 +16,18 @@ const i18nService: I18nService = I18nService.getInstance();
 
 export const ChangelogModal: ModalComponent<void, void> = () => {
 
+  const linkOpener: LinkOpenerService = LinkOpenerService.getInstance();
+  const openGithub = () => linkOpener.open("https://github.com/Zagrios/bs-manager");
+  const openTwitter = () => linkOpener.open("https://twitter.com/BSManager_");
+  const openSupportPage = () => linkOpener.open("https://www.patreon.com/bsmanager");
+  const openDiscord = () => linkOpener.open("https://discord.gg/uSqbHVpKdV");
+
   const [languageSelected] = useState(i18nService.currentLanguage.split("-")[0]);
   const [changelogData, setChangelogData] = useState<jsonData>({
     title: null,
     body: null,
     version: null
   });
-
 
   useEffect(() => {
     fetch(`https://raw.githubusercontent.com/Zagrios/bs-manager/ressources/jsons/Changelog/${languageSelected}.json`)
@@ -36,15 +42,15 @@ export const ChangelogModal: ModalComponent<void, void> = () => {
       <BsmImage className="mx-auto h-20" image={BeatRunning} />
       <div className="overflow-y-scroll h-80 content grow" dangerouslySetInnerHTML={{ __html: changelogData.body }}/>
       <span className="block w-[60%] mx-auto my-[16px] h-[2px] rounded-full bg-main-color-2" />
-      <div className="mt-1 flex flex-row justify-between">
-        <div className="flex gap-2">
-          <BsmIcon icon='github' className='text-blue-500 h-6' style={{ color: "black" }} />
-          <BsmIcon icon='twitter' className='text-blue-500 h-6 ' style={{ color: "#00acee" }} />
-          <BsmIcon icon='patreon' className='text-blue-500 h-6 ' style={{ color: "#f96854" }} />
-          <BsmIcon icon='discord' className='text-blue-500 h-5 ' style={{ color: "#7289d9" }} />
+      <div className=" flex flex-row justify-between">
+        <div className="flex gap-1">
+          <BsmButton onClick={openGithub} className=" rounded-md h-6 p-1 bg-transparent" icon="github" withBar={false}/>
+          <BsmButton onClick={openTwitter} className=" rounded-md h-6 p-1" icon="twitter" withBar={false}/>
+          <BsmButton onClick={openSupportPage} className=" rounded-md h-6 p-1" icon="patreon" withBar={false}/>
+          <BsmButton onClick={openDiscord} className=" rounded-md h-6 p-[5px]" icon="discord" withBar={false}/>
         </div>
-        <span>{changelogData.version}</span>    
+        <span><i>{changelogData.version}</i></span>    
       </div>
     </form>
   )
-}
+}   
