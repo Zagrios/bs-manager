e5142fe1317dbc572623f824c8c37b9fe9abec5d
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2023-04-01 18:10:52 +0200

    [feature] add german translation

---
+++ src/renderer/components/svgs/flags/german-icon.component.tsx
@@ -0,0 +1,20 @@
+import { CSSProperties } from "react";
+
+export function GermanIcon(props: {className?: string, style?: CSSProperties}) {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg"  width="71px" height="48px" viewBox="0 0 71 48" version="1.1" className={props.className} style={props.style}>
+            <defs/>
+            <g id="Flags" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" transform="translate(-239.000000, -805.000000)">
+                <g transform="translate(70.000000, 70.000000)" fill-rule="nonzero" id="Germany">
+                    <g transform="translate(169.000000, 735.000000)">
+                        <g id="Bolivia-9">
+                            <path d="M0.52,7 L0.52,16 L70.52,16 L70.52,7 C70.52,3.33282926 67.5471707,0.36 63.88,0.36 L7.17,0.36 C5.40723053,0.357345224 3.71575038,1.05574249 2.46834517,2.3012719 C1.22093997,3.54680131 0.519998001,5.23722853 0.52,7 Z" id="Shape" fill="#434A54"/>
+                            <polygon id="Shape" fill="#ED5565" points="0.52 31.74 70.52 31.74 70.52 16.07 0.52 16.07"/>
+                            <path d="M0.52,40.76 C0.52,44.4271707 3.49282926,47.4 7.16,47.4 L63.88,47.4 C67.5471707,47.4 70.52,44.4271707 70.52,40.76 L70.52,31.76 L0.52,31.76 L0.52,40.76 Z" id="Shape" fill="#F6BB42"/>
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    )
+}
