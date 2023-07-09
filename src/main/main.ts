215586a41b5faedd10d9daee67c5c15d7df6da71
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2023-07-09 18:49:08 +0200

    [feature] nothing

--- src/main/main.ts
+++ src/main/main.ts
@@ -1,5 +1,3 @@
-/* eslint global-require: off, no-console: off, promise/always-return: off */
-
 /**
  * This module executes inside of electron's main process. You can start
  * electron renderer process from here and communicate with the other processes
