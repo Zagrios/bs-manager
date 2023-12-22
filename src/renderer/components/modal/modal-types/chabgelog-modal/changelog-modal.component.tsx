0776de8d7994b37fbc6a6707b30b487677d9f283
Shidorien <Gaetangerard@hotmail.fr>
2023-12-22 01:24:37 +0100

    Remove unused imports and styling in ChangelogModal component

--- src/renderer/components/modal/modal-types/chabgelog-modal/changelog-modal.component.tsx
+++ src/renderer/components/modal/modal-types/chabgelog-modal/changelog-modal.component.tsx
@@ -1,9 +1,7 @@
 import { BsmButton } from "renderer/components/shared/bsm-button.component";
-import { BsmImage } from "renderer/components/shared/bsm-image.component";
 import { ChangelogVersion } from "renderer/services/auto-updater.service";
 import { LinkOpenerService } from "renderer/services/link-opener.service";
 import { ModalComponent } from "renderer/services/modale.service";
-import BeatRunning from "../../../../../../assets/images/apngs/beat-running.png";
 import DOMPurify from 'dompurify';
 import './changelog-modal.component.css';
 
@@ -19,7 +17,6 @@ export const ChangelogModal: ModalComponent<void, ChangelogVersion> = ({ resolve
     return (
       <form className="w-[350px] text-gray-800 dark:text-gray-200 h-[70vh] flex flex-col justify-between">
         <h1 className=" p-4 pt-1 text-3xl uppercase tracking-wide w-full text-center text-gray-800 dark:text-gray-200 font-bold">{changelog?.title}</h1>
-        <span className="block w-[100%] mx-auto m-0 h-1 rounded-full bg-main-color-1" />
         <div className=" overflow-y-scroll h-full content grow" dangerouslySetInnerHTML={{ __html: DOMPurify.sanitize(changelog?.htmlBody) }}/>
         <span className="block w-[100%] mx-auto mt-0 mb-4 h-1 rounded-full bg-main-color-1" />
         <div className="flex flex-row justify-between">
