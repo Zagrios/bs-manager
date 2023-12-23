b91d74f3a3b2e352b87a6b93e36bdd1a7f38379d
shidorien <gaetangerard@hotmail.fr>
2023-12-23 22:38:38 +0100

    Fix xlink namespace in PatreonIcon component

--- src/renderer/components/svgs/icons/patreon-icon.component.tsx
+++ src/renderer/components/svgs/icons/patreon-icon.component.tsx
@@ -2,7 +2,7 @@ import { CSSProperties } from "react";
 
 export default function PatreonIcon(props: { className?: string; style?: CSSProperties }) {
     return (
-        <svg className={props.className} style={props.style} xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 1080 1080" xml:space="preserve" fill="currentColor">
+        <svg className={props.className} style={props.style} xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" version="1.1" id="Layer_1" x="0px" y="0px" viewBox="0 0 1080 1080" xmlSpace="preserve" fill="currentColor">
             <path  d="M1033.05,324.45c-0.19-137.9-107.59-250.92-233.6-291.7c-156.48-50.64-362.86-43.3-512.28,27.2  C106.07,145.41,49.18,332.61,47.06,519.31c-1.74,153.5,13.58,557.79,241.62,560.67c169.44,2.15,194.67-216.18,273.07-321.33  c55.78-74.81,127.6-95.94,216.01-117.82C929.71,603.22,1033.27,483.3,1033.05,324.45z"/>
         </svg>
     );
