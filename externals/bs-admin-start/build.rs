4c8617a419413d368a120fc485c69feb9d264ad7
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-01-20 15:42:30 +0100

    [bugfix] Start BS as Admin when Steam is running as admin but BSM is not

---
+++ externals/bs-admin-start/build.rs
@@ -0,0 +1,20 @@
+extern crate winres;
+
+fn main() {
+  if cfg!(target_os = "windows") {
+    let mut res = winres::WindowsResource::new();
+    res.set_manifest(r#"
+      <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+      <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+          <security>
+              <requestedPrivileges>
+                  <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+              </requestedPrivileges>
+          </security>
+      </trustInfo>
+      </assembly>
+    "#);
+    res.set_icon("./icon.ico");
+    res.compile().unwrap();
+  }
+}
--- src/main/helpers/os.helpers.ts
+++ src/main/helpers/os.helpers.ts
@@ -11,3 +11,15 @@ export async function taskRunning(task: string): Promise<boolean> {
         return false;
     }
 }
+
+export async function getProcessPid(task: string): Promise<number> {
+    try {
+        const processes = await psList();
+        const process = processes.find(process => process.name?.includes(task) || process.cmd?.includes(task));
+        return process?.pid;
+    }
+    catch(error){
+        log.error(error);
+        return null;
+    }
+}
