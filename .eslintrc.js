792ad9d777ee322d1994b98a8861c274c7629f42
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-01-21 16:54:53 +0100

    [chore] fix lint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,8 +46,12 @@ module.exports = {
         "react/prop-types": "off",
         "import/no-cycle": "off",
         "prefer-promise-reject-errors": "off",
-        "react/jsx-no-target-blank": "off"
-        "@typescript-eslint/ban-types": "off"
+        "react/jsx-no-target-blank": "off",
+        "@typescript-eslint/ban-types": ["error", {
+            types: {
+                Function: false,
+            }
+        }]
     },
     parserOptions: {
         ecmaVersion: 2020,
