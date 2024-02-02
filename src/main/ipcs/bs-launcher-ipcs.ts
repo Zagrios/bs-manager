cdc589f602f1e4d253d6d152349e6edfc710613e
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-02-02 20:18:33 +0100

    [bugfix] handle error when cheking if steam is running as admin

--- src/main/ipcs/bs-launcher-ipcs.ts
+++ src/main/ipcs/bs-launcher-ipcs.ts
@@ -6,6 +6,7 @@ import { from } from "rxjs";
 import { SteamLauncherService } from "../services/bs-launcher/steam-launcher.service";
 import { OculusLauncherService } from "../services/bs-launcher/oculus-launcher.service";
 import { SteamService } from "../services/steam.service";
+import log from "electron-log";
 import isElevated from "is-elevated";
 
 const ipc = IpcService.getInstance();
@@ -19,9 +20,12 @@ ipc.on<boolean>("bs-launch.need-start-as-admin", (_, reply) => {
     const steam = SteamService.getInstance();
     reply(from(isElevated().then(elevated => {
         if(elevated){ return false; }
-        return steam.isElevated();
+        return steam.isElevated().catch(e => {
+            log.error("Error while checking if Steam is running as admin", e);
+            return false;
+        });
     })));
-})
+});
 
 ipc.on<LaunchOption>("create-launch-shortcut", (req, reply) => {
     const bsLauncher = BSLauncherService.getInstance();
