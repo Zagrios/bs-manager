90bb8f2c19afc4ac89bff7db40c40d8b2426a075
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-02-03 23:22:28 +0100

    Fix eslint rule for no-shadow

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,6 +55,7 @@ module.exports = {
         "no-unused-vars": "off",
         "@typescript-eslint/no-unused-vars": "error",
         'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+        "no-shadow": "off",
     },
     parserOptions: {
         ecmaVersion: 2020,
