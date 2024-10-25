38e0cc56d0be4b034598b3b8f207fa64ec1a8aa2
silentrald <silentrald1216@gmail.com>
2024-10-25 19:05:39 +0800

    [chore] add write permissions to bs-versions.json after install

---
+++ build/after-install.sh
@@ -0,0 +1,3 @@
+# Add write permissions to anybody so that this can be sync with the
+#   github's bs-versions.json when starting bsmanager
+/usr/bin/chmod +002 /opt/BSManager/resources/assets/jsons/bs-versions.json
