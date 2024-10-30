e6bdf56c6d6dc50d06e5cf53c9782743d835f3bb
silentrald <silentrald1216@gmail.com>
2024-10-30 08:13:11 +0800

    [chore] move build config to electron-builder.ts for better typing

---
+++ electron-builder.ts
@@ -0,0 +1,83 @@
+import { Configuration } from "electron-builder";
+
+const config: Configuration = {
+    extraResources: [
+        "./assets/jsons/bs-versions.json",
+        "./assets/jsons/patreons.json",
+        "./assets/proto/song_details_cache_v1.proto"
+    ],
+    productName: "BSManager",
+    appId: "org.erb.BSManager",
+    asarUnpack: "**\\*.{node,dll}",
+    files: [
+        "dist/**/*",
+        "node_modules",
+        "package.json"
+    ],
+    afterSign: ".erb/scripts/notarize.js",
+    afterPack: ".erb/scripts/after-pack.js",
+    win: {
+        signingHashAlgorithms: ["sha256"],
+        certificateSha1: "2164d6a7d641ecf6ad57852f665a518ca2bf960f",
+        target: [
+            "nsis",
+            "nsis-web"
+        ],
+        icon: "./build/icons/win/favicon.ico",
+        extraResources: [
+            "./build/icons/win",
+            "./assets/scripts/*.exe"
+        ],
+    },
+    linux: {
+        target: [
+            "deb",
+            "rpm",
+            "pacman"
+        ],
+        icon: "./build/icons/png",
+        category: "Utility;Game;",
+        extraResources: [
+            "./build/icons/png",
+            "./assets/scripts/DepotDownloader"
+        ],
+        protocols: {
+            name: "BSManager",
+            schemes: [
+                "bsmanager",
+                "beatsaver",
+                "bsplaylist",
+                "modelsaber",
+                "web+bsmap",
+            ],
+        },
+    },
+    deb: {
+        fpm: ["--after-install=build/after-install.sh"],
+    },
+    rpm: {
+        fpm: ["--after-install=build/after-install.sh"],
+    },
+    pacman: {
+        fpm: ["--after-install=build/after-install.sh"],
+    },
+    directories: {
+        app: "release/app",
+        buildResources: "assets",
+        output: "release/build",
+    },
+    publish: {
+        provider: "github",
+        owner: "Zagrios",
+    },
+    fileAssociations: [
+        {
+            ext: "bplist",
+            description: "Beat Saber Playlist (BSManager)",
+            icon: "./assets/bsm_file.ico",
+            role: "Viewer",
+        },
+    ],
+};
+
+export default config;
