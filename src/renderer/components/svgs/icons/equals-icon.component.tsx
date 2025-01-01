ce19458e40d7e5426acccf60b86c0153ab2dc3d3
silentrald <silentrald1216@gmail.com>
2025-01-01 11:37:13 +0800

    [feat-684] updated icon component to use createSvgIcon

--- src/renderer/components/svgs/icons/equals-icon.component.tsx
+++ src/renderer/components/svgs/icons/equals-icon.component.tsx
@@ -1,8 +1,10 @@
-import { CSSProperties } from "react";
+import { createSvgIcon } from "../svg-icon.type";
 
-export function EqualIcon(props: Readonly<{ className?: string; style?: CSSProperties }>) {
+export const EqualIcon = createSvgIcon((props, ref) => {
     return (
         <svg
+            ref={ref}
+            {...props}
             className={props.className}
             style={props.style}
             xmlns="http://www.w3.org/2000/svg"
@@ -12,4 +14,4 @@ export function EqualIcon(props: Readonly<{ className?: string; style?: CSSPrope
             <path d="M220-280q-25 0-42.5-17.5T160-340q0-25 17.5-42.5T220-400h520q25 0 42.5 17.5T800-340q0 25-17.5 42.5T740-280H220Zm0-280q-25 0-42.5-17.5T160-620q0-25 17.5-42.5T220-680h520q25 0 42.5 17.5T800-620q0 25-17.5 42.5T740-560H220Z" />
         </svg>
     );
-}
+});
