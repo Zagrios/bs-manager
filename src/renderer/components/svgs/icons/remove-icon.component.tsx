d8b853211032735aaff406bb42130a0f33289dd3
silentrald <silentrald1216@gmail.com>
2025-01-01 11:37:13 +0800

    [feat-684] updated icon component to use createSvgIcon

--- src/renderer/components/svgs/icons/remove-icon.component.tsx
+++ src/renderer/components/svgs/icons/remove-icon.component.tsx
@@ -1,10 +1,10 @@
-import { CSSProperties } from "react";
+import { createSvgIcon } from "../svg-icon.type"
 
-export function RemoveIcon(props: Readonly<{ className?: string; style?: CSSProperties }>) {
+export const RemoveIcon = createSvgIcon((props, ref) => {
     return (
         <svg
-            className={props.className}
-            style={props.style}
+            ref={ref}
+            {...props}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 -960 960 960"
             fill="currentColor"
@@ -12,4 +12,4 @@ export function RemoveIcon(props: Readonly<{ className?: string; style?: CSSProp
             <path d="M240-440q-17 0-28.5-11.5T200-480q0-17 11.5-28.5T240-520h480q17 0 28.5 11.5T760-480q0 17-11.5 28.5T720-440H240Z" />
         </svg>
     )
-}
+});
