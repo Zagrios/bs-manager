f2ad6c15317c005e348366d4d1a59fa78b56e117
silentrald <silentrald1216@gmail.com>
2025-01-09 23:55:17 +0800

    [bugfix-733] fix issue with unit tests

--- src/__tests__/unit/os.test.ts
+++ src/__tests__/unit/os.test.ts
@@ -24,7 +24,7 @@ jest.mock("electron-log", () => ({
     info: jest.fn(),
     error: jest.fn(),
 }));
-jest.mock("ps-list", () => () => []);
+jest.mock("ps-list", (): (() => any[]) => () => []);
 
 const IS_WINDOWS = process.platform === "win32";
 const IS_LINUX = process.platform === "linux";
