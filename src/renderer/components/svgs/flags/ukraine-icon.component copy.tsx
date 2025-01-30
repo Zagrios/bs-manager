abacf5f628a60290e03672a3f6ebe2c43bd33359
HighError <hierrorua@gmail.com>
2025-01-30 16:15:14 +0200

    Fixed rounded flag

--- src/renderer/components/svgs/flags/ukraine-icon.component copy.tsx
+++ src/renderer/components/svgs/flags/ukraine-icon.component copy.tsx
@@ -1,12 +1,12 @@
 import { CSSProperties } from "react";
 
+// Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools
+
 export function UkraineIcon(props: { className?: string; style?: CSSProperties }) {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="640" height="480" viewBox="0 0 640 480"  className={props.className} style={props.style}>
-    <g fill-rule="evenodd" stroke-width="1">
-        <path fill="#ffd700" d="M0 0h640v480H0z" />
-        <path fill="#0057b8" d="M0 0h640v240H0z" />
-    </g>
+        <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="71" height="48" viewBox="0 5 36 26" className={props.className} style={props.style}>
+    <path fill="#005BBB" d="M32 5H4a4 4 0 0 0-4 4v9h36V9a4 4 0 0 0-4-4z"></path>
+    <path fill="#FFD500" d="M36 27a4 4 0 0 1-4 4H4a4 4 0 0 1-4-4v-9h36v9z"></path>
 </svg>
     );
 }
