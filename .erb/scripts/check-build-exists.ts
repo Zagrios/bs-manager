efcb914d1499a31d6944612bb2671c0b2f2ae0c5
Zagrios <40181755+Zagrios@users.noreply.github.com>
2026-08-23 00:55:27 +0200

    Migrate package management to pnpm

--- .erb/scripts/check-build-exists.ts
+++ .erb/scripts/check-build-exists.ts
@@ -8,9 +8,9 @@ const mainPath = path.join(paths.distMainPath, "main.js");
 const rendererPath = path.join(paths.distRendererPath, "index.html");
 
 if (!fs.existsSync(mainPath)) {
-    throw new Error(chalk.whiteBright.bgRed.bold('The main process is not built yet. Build it by running "npm run build"'));
+    throw new Error(chalk.whiteBright.bgRed.bold('The main process is not built yet. Build it by running "pnpm run build"'));
 }
 
 if (!fs.existsSync(rendererPath)) {
-    throw new Error(chalk.whiteBright.bgRed.bold('The renderer process is not built yet. Build it by running "npm run build"'));
+    throw new Error(chalk.whiteBright.bgRed.bold('The renderer process is not built yet. Build it by running "pnpm run build"'));
 }
--- .erb/scripts/check-port-in-use.js
+++ .erb/scripts/check-port-in-use.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || "1212";
 
 detectPort(port, (err, availablePort) => {
     if (port !== String(availablePort)) {
-        throw new Error(chalk.whiteBright.bgRed.bold(`Port "${port}" on "localhost" is already in use. Please use another port. ex: PORT=4343 npm start`));
+        throw new Error(chalk.whiteBright.bgRed.bold(`Port "${port}" on "localhost" is already in use. Please use another port. ex: PORT=4343 pnpm start`));
     } else {
         process.exit(0);
     }
